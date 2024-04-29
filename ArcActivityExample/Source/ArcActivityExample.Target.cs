0bda376661d9add07fbe9ca399f23e77d79e9c9d
Garrett Fleenor <kylegar@kylegar.net>
2024-04-29 00:09:41 -0700

    Updates for 5.4

--- ArcActivityExample/Source/ArcActivityExample.Target.cs
+++ ArcActivityExample/Source/ArcActivityExample.Target.cs
@@ -8,7 +8,7 @@ public class ArcActivityExampleTarget : TargetRules
 	public ArcActivityExampleTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ArcActivityExample");
 	}
